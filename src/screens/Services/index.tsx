b4a037a09e63ae1e3359cb40b83a73fe123c904a
brunomacedo <contato@brunomacedo.com.br>
2022-02-11 16:13:17 -0300

    fix: keyboard view when input quantity

--- src/screens/Services/index.tsx
+++ src/screens/Services/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import {
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  View,
+} from "react-native";
 import Card from "../../components/Card";
 import { MOCKS } from "../../mocks";
 import { colors, theme } from "../../theme/global";
@@ -7,19 +13,26 @@ import { colors, theme } from "../../theme/global";
 export function Services() {
   return (
     <View style={theme.container}>
-      <FlatList
-        bounces={false}
-        data={MOCKS.services}
-        renderItem={({ item }) => <Card item={item} />}
-        ItemSeparatorComponent={() => <View style={theme.separator} />}
-        ListFooterComponent={() => <View style={theme.separator} />}
-        stickyHeaderIndices={[0]}
-        ListHeaderComponent={() => (
-          <View style={theme.header}>
-            <Text style={{ color: colors.white, fontSize: 22 }}>Services</Text>
-          </View>
-        )}
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={theme.container}
+      >
+        <FlatList
+          bounces={false}
+          data={MOCKS.services}
+          renderItem={({ item }) => <Card item={item} />}
+          ItemSeparatorComponent={() => <View style={theme.separator} />}
+          ListFooterComponent={() => <View style={theme.separator} />}
+          stickyHeaderIndices={[0]}
+          ListHeaderComponent={() => (
+            <View style={theme.header}>
+              <Text style={{ color: colors.white, fontSize: 22 }}>
+                Services
+              </Text>
+            </View>
+          )}
+        />
+      </KeyboardAvoidingView>
     </View>
   );
 }
